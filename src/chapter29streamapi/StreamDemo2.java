85df9a140b81c0b281594688db52d69481c38538
JHerng <jiaherng2001@gmail.com>
2020-08-28 19:29:21 +0800

    Initial commit.

--- src/chapter29streamapi/StreamDemo2.java
+++ src/chapter29streamapi/StreamDemo2.java
@@ -1,2 +1,32 @@
-package chapter29streamapi;public class StreamDemo2 {
+// Demonstrate the reduce() method.
+
+package chapter29streamapi;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+class StreamDemo2 {
+    public static void main(String[] args) {
+
+        // Create a list of Integer values.
+        ArrayList<Integer> myList = new ArrayList<>();
+
+        myList.add(7);
+        myList.add(18);
+        myList.add(10);
+        myList.add(24);
+        myList.add(17);
+        myList.add(5);
+
+        // Two ways to obtain the integer product of the elements in myList
+        // by use of reduce().
+        Optional<Integer> productObj = myList.stream().reduce((a, b) -> a * b);
+        if (productObj.isPresent()) System.out.println("Product as Optional: " + productObj.get());
+
+        int product = myList.stream().reduce(1, (a, b) -> a * b);
+        System.out.println("Product as int: " + product);
+
+        int evenProduct = myList.stream().reduce(1, (a, b) -> ((b % 2) == 0) ? a * b : a);
+        System.out.println("Product of even number as int: " + evenProduct);
+    }
 }
--- src/chapter29streamapi/StreamDemo3.java
+++ src/chapter29streamapi/StreamDemo3.java
@@ -1,2 +1,40 @@
-package chapter29streamapi;public class StreamDemo3 {
+// Demonstrate the use of a combiner with reduce().
+
+package chapter29streamapi;
+
+import java.util.ArrayList;
+
+class StreamDemo3 {
+    public static void main(String[] args) {
+
+        // This is a list of double values.
+        ArrayList<Double> myList = new ArrayList<>();
+
+        myList.add(7.0);
+        myList.add(18.0);
+        myList.add(10.0);
+        myList.add(24.0);
+        myList.add(17.0);
+        myList.add(5.0);
+
+        double productOfSqrRoots = myList.parallelStream().reduce(
+                1.0,
+                (a, b) -> a * Math.sqrt(b),
+                (a, b) -> a * b
+        );
+
+        System.out.println("Product of square roots: " + productOfSqrRoots);
+
+
+        // This won't work. !! VERY HARD TO UNDERSTAND !!
+        // In this version of reduce(), ACCUMULATOR and COMBINER function are one and the same.
+        // This results in an error because when TWO PARTIAL RESULTS ARE COMBINED, THEIR SQUARE
+        // ROOTS ARE MULTIPLIED TOGETHER RATHER THAN THE PARTIAL RESULTS, themselves.
+        double productOfSqrRoots2 = myList.parallelStream().reduce(
+                1.0,
+                (a, b) -> a * Math.sqrt(b)
+        );
+
+        System.out.println(productOfSqrRoots2);
+    }
 }
